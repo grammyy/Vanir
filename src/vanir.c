657e82192fea28fbbf9a703e095220143de79164
Elias-bff <bartender.winery@gmail.com>
2024-01-07 23:37:46 -0500

    removed unused headers

--- src/vanir.c
+++ src/vanir.c
@@ -1,20 +1,9 @@
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
-#include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "hooks.h"
 #include "input.h"
-
-static int l_messagebox(lua_State * L) {
-	const char * s = luaL_checkstring(L, 1);
-
-	MessageBox(NULL,s,"messagebox",MB_OK);
-
-	return 0;
-}
 
 typedef struct {
     const char* name;
@@ -30,12 +19,7 @@ void registerModules(lua_State* L, const luaL_reg* funcs) {
 }
 
 __declspec(dllexport) int luaopen_vanir(lua_State * L) {
-	//pid_t pid = getpid();
-    
     luaL_dofile(L,"preload.lua");
-
-	lua_pushcfunction(L, l_messagebox);
-	lua_setglobal(L,"messagebox");
 
 	const luaL_reg luaReg[] = {
 		{"hooks", hooksInit},
@@ -43,8 +27,7 @@ __declspec(dllexport) int luaopen_vanir(lua_State * L) {
         {NULL, NULL}
     };
 
-    // Register the "vanir" table with functions
     registerModules(L, luaReg);
-    //runHook(&think, L);
+
 	return 1;
 }
