f8ca11d872f1ecf5420381a628024cf2c929b059
Elias-bff <bartender.winery@gmail.com>
2024-02-23 22:41:34 -0500

    render enums now use a bit array instead of a switch statement

--- src/modules/render.c
+++ src/modules/render.c
@@ -8,60 +8,93 @@
 #include "../graphics/render.h"
 #include "../vanir.h"
 
-GLenum blendSwitch(int blend) {
-    switch (blend) { //convert these to structs later
-        case 15: return GL_SRC_ALPHA_SATURATE;
-        case 14: return GL_ONE_MINUS_CONSTANT_ALPHA;
-        case 13: return GL_CONSTANT_ALPHA;
-        case 12: return GL_ONE_MINUS_CONSTANT_COLOR;
-        case 11: return GL_CONSTANT_COLOR;
-        case 10: return GL_ONE_MINUS_DST_ALPHA;
-        case 9:  return GL_DST_ALPHA;
-        case 8:  return GL_ONE_MINUS_SRC_ALPHA;
-        case 7:  return GL_SRC_ALPHA;
-        case 6:  return GL_ONE_MINUS_DST_COLOR;
-        case 5:  return GL_DST_COLOR;
-        case 4:  return GL_ONE_MINUS_SRC_COLOR;
-        case 3:  return GL_SRC_COLOR;
-        case 2:  return GL_ONE;
-        case 1:  return GL_ZERO;
-        default: return GL_INVALID_ENUM;
-    }
-}
+#define blendEnums 15
+#define capEnums 28
+#define targetEnums 4
+#define qualityEnums 28
+#define groupEnums 10
+#define bufferEnums 3
 
-GLenum capSwitch(int cap) {
-    switch (cap) { //convert these to structs later
-        case 28: return GL_BLEND;
-        //case 27: return GL_CLIP_DISTANCE; undefined for some reason
-        case 26: return GL_COLOR_LOGIC_OP;
-        case 25: return GL_CULL_FACE;
-        case 24: return GL_DEBUG_OUTPUT;
-        case 23: return GL_DEBUG_OUTPUT_SYNCHRONOUS;
-        case 22: return GL_DEPTH_CLAMP;
-        case 21: return GL_DEPTH_TEST;
-        case 20: return GL_DITHER;
-        case 19: return GL_FRAMEBUFFER_SRGB;
-        case 18: return GL_LINE_SMOOTH;
-        case 17: return GL_MULTISAMPLE;
-        case 16: return GL_POLYGON_OFFSET_FILL;
-        case 15: return GL_POLYGON_OFFSET_LINE;
-        case 14: return GL_POLYGON_OFFSET_POINT;
-        case 13: return GL_POLYGON_SMOOTH;
-        case 12: return GL_PRIMITIVE_RESTART;
-        case 11: return GL_PRIMITIVE_RESTART_FIXED_INDEX;
-        case 10: return GL_RASTERIZER_DISCARD;
-        case 9:  return GL_SAMPLE_ALPHA_TO_COVERAGE;
-        case 8:  return GL_SAMPLE_ALPHA_TO_ONE;
-        case 7:  return GL_SAMPLE_COVERAGE;
-        case 6:  return GL_SAMPLE_SHADING;
-        case 5:  return GL_SAMPLE_MASK;
-        case 4:  return GL_SCISSOR_TEST;
-        case 3:  return GL_STENCIL_TEST;
-        case 2:  return GL_TEXTURE_CUBE_MAP_SEAMLESS;
-        case 1:  return GL_PROGRAM_POINT_SIZE;
-        default: return GL_INVALID_ENUM;
-    }
-}
+const GLenum blendLookup[blendEnums] = {
+    GL_ZERO,
+    GL_ONE,
+    GL_SRC_COLOR,
+    GL_ONE_MINUS_SRC_COLOR,
+    GL_DST_COLOR,
+    GL_ONE_MINUS_DST_COLOR,
+    GL_SRC_ALPHA,
+    GL_ONE_MINUS_SRC_ALPHA,
+    GL_DST_ALPHA,
+    GL_ONE_MINUS_DST_ALPHA,
+    GL_CONSTANT_COLOR,
+    GL_ONE_MINUS_CONSTANT_COLOR,
+    GL_CONSTANT_ALPHA,
+    GL_ONE_MINUS_CONSTANT_ALPHA,
+    GL_SRC_ALPHA_SATURATE
+};
+
+const GLenum capLookup[capEnums] = {
+    GL_PROGRAM_POINT_SIZE,
+    GL_TEXTURE_CUBE_MAP_SEAMLESS,
+    GL_STENCIL_TEST,
+    GL_SCISSOR_TEST,
+    GL_SAMPLE_MASK,
+    GL_SAMPLE_SHADING,
+    GL_SAMPLE_COVERAGE,
+    GL_SAMPLE_ALPHA_TO_ONE,
+    GL_SAMPLE_ALPHA_TO_COVERAGE,
+    GL_RASTERIZER_DISCARD,
+    GL_PRIMITIVE_RESTART_FIXED_INDEX,
+    GL_PRIMITIVE_RESTART,
+    GL_POLYGON_SMOOTH,
+    GL_POLYGON_OFFSET_POINT,
+    GL_POLYGON_OFFSET_LINE,
+    GL_POLYGON_OFFSET_FILL,
+    GL_MULTISAMPLE,
+    GL_LINE_SMOOTH,
+    GL_FRAMEBUFFER_SRGB,
+    GL_DITHER,
+    GL_DEPTH_TEST,
+    GL_DEPTH_CLAMP,
+    GL_DEBUG_OUTPUT_SYNCHRONOUS,
+    GL_DEBUG_OUTPUT,
+    GL_CULL_FACE,
+    GL_COLOR_LOGIC_OP,
+    // Placeholder for 27: GL_CLIP_DISTANCE,
+    GL_BLEND,
+};
+
+const GLenum targetLookup[targetEnums] = {
+    GL_TEXTURE_COMPRESSION_HINT,
+    GL_POLYGON_SMOOTH_HINT,
+    GL_LINE_SMOOTH_HINT,
+    GL_FRAGMENT_SHADER_DERIVATIVE_HINT,
+};
+
+const GLenum qualityLookup[qualityEnums] = {
+    GL_DONT_CARE,
+    GL_NICEST,
+    GL_FASTEST,
+};
+
+const GLenum groupLookup[groupEnums] = {
+    GL_POLYGON,
+    GL_QUAD_STRIP,
+    GL_QUADS,
+    GL_TRIANGLE_FAN,
+    GL_TRIANGLE_STRIP,
+    GL_TRIANGLES,
+    GL_LINE_LOOP,
+    GL_LINE_STRIP,
+    GL_LINES,
+    GL_POINTS,
+};
+
+const GLenum bufferLookup[bufferEnums] = {
+    GL_COLOR_BUFFER_BIT,
+    GL_DEPTH_BUFFER_BIT,
+    GL_STENCIL_BUFFER_BIT,
+};
 
 // window methods ↓↓↓ window methods ///
 int selectRender(lua_State *L) {
@@ -158,20 +191,7 @@ int setQuality(lua_State *L) {
     int target = luaL_checkinteger(L, 1);
     int quality = luaL_checkinteger(L, 2);
 
-    switch (target) { //convert these to structs later
-        case 4: target = GL_FRAGMENT_SHADER_DERIVATIVE_HINT; break;
-        case 3: target = GL_LINE_SMOOTH_HINT; break;
-        case 2: target = GL_POLYGON_SMOOTH_HINT; break;
-        case 1: target = GL_TEXTURE_COMPRESSION_HINT; break;
-    }
-
-    switch (quality) {
-        case 3: quality = GL_FASTEST; break;
-        case 2: quality = GL_NICEST; break;
-        case 1: quality = GL_DONT_CARE; break;
-    }
-
-    glHint(target, quality);
+    glHint(targetLookup[target], qualityLookup[quality]);
 
     return 0;
 }
@@ -180,28 +200,23 @@ int setBlend(lua_State *L) {
     int source = luaL_checkinteger(L, 1);
     int blend = luaL_checkinteger(L, 2);
 
-    source=blendSwitch(source);
-    blend=blendSwitch(blend);
-    
-    glBlendFunc(source, blend);
+    glBlendFunc(blendLookup[source], blendLookup[blend]);
 
     return 0;
 }
 
 int enable(lua_State *L) {
     int cap = luaL_checkinteger(L, 1);
-    cap=capSwitch(cap);
 
-    glEnable(cap);
+    glEnable(capLookup[cap]);
 
     return 0;
 }
 
 int disable(lua_State *L) {
     int cap = luaL_checkinteger(L, 1);
-    cap=capSwitch(cap);
 
-    glDisable(cap);
+    glDisable(capLookup[cap]);
 
     return 0;
 }
@@ -212,14 +227,8 @@ int clear(lua_State *L) {
 
     int buffer = lua_tointeger(L, 2);
     
-    switch (buffer) { //convert these to structs later
-        case 3: buffer = GL_STENCIL_BUFFER_BIT; break;
-        case 2: buffer = GL_DEPTH_BUFFER_BIT; break;
-        case 1: default: buffer = GL_COLOR_BUFFER_BIT; break;
-    }
-
     glClearColor(color.r, color.g, color.b, color.a);
-    glClear(buffer);
+    glClear(bufferLookup[buffer]);
     
     return 0;
 }
@@ -233,20 +242,7 @@ int force(lua_State *L) {
 int begin(lua_State *L) {
     int group = luaL_checkinteger(L, 1);
 
-    switch (group) { //convert these to structs later
-        case 10: group = GL_POINTS; break;
-        case 9: group = GL_LINES; break;
-        case 8: group = GL_LINE_STRIP; break;
-        case 7: group = GL_LINE_LOOP; break;
-        case 6: group = GL_TRIANGLES; break;
-        case 5: group = GL_TRIANGLE_STRIP; break;
-        case 4: group = GL_TRIANGLE_FAN; break;
-        case 3: group = GL_QUADS; break;
-        case 2: group = GL_QUAD_STRIP; break;
-        case 1: group = GL_POLYGON; break;
-    }
-
-    glBegin(group);
+    glBegin(groupLookup[group]);
 
     return 0;
 }
